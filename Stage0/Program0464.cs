192cf490831ff6b66e83f159dbb209488023fab3
EstiRosen <estirphoto@gmail.com>
2023-12-25 16:56:26 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program0464.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        welcome0464();
+        welcome1940();
+        Console.ReadKey();
+    }
+
+    static partial void welcome1940();
+    private static void welcome0464()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+}
