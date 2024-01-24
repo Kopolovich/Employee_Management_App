28441a83419eed37324bc9f71d5524e5c3c58564
Kopolovich <noabrako6@gmail.com>
2024-01-24 20:15:11 +0200

    stage 4- starting engineer implementation

---
+++ BL/BO/Exceptions.cs
@@ -0,0 +1,63 @@
+﻿namespace BO;
+
+/// <summary>
+/// exception for non existing item
+/// </summary>
+[Serializable]
+public class BlDoesNotExistException : Exception
+{
+    public BlDoesNotExistException(string? message) : base(message) { }
+    public BlDoesNotExistException(string? message, Exception innerException) 
+                                                    : base(message, innerException) { }
+}
+
+/// <summary>
+/// exception for already existing item
+/// </summary>
+[Serializable]
+public class BlAlreadyExistsException : Exception
+{
+    public BlAlreadyExistsException(string? message) : base(message) { }
+    public BlAlreadyExistsException(string? message, Exception innerException)
+                                                    : base(message, innerException) { }
+}
+
+/// <summary>
+/// exception for impossilbe deletion
+/// </summary>
+[Serializable]
+public class BlDeletionImpossibleException : Exception
+{
+    public BlDeletionImpossibleException(string? message) : base(message) { }
+    public BlDeletionImpossibleException(string? message, Exception innerException)
+                                                   : base(message, innerException) { }
+}
+
+/// <summary>
+/// exception for failing to create xml file
+/// </summary>
+[Serializable]
+public class BlXMLFileLoadCreateException : Exception
+{
+    public BlXMLFileLoadCreateException(string? message) : base(message) { }
+    public BlXMLFileLoadCreateException(string? message, Exception innerException)
+                                                  : base(message, innerException) { }
+}
+
+/// <summary>
+/// exception for trying to use property with null value
+/// </summary>
+[Serializable]
+public class BlNullPropertyException : Exception
+{
+    public BlNullPropertyException(string? message) : base(message) { }
+}
+
+/// <summary>
+/// exception for trying to use property with null value
+/// </summary>
+[Serializable]
+public class BlInvalidValueException : Exception
+{
+    public BlInvalidValueException(string? message) : base(message) { }
+}
--- BL/BlImplementation/EngineerImplementation.cs
+++ BL/BlImplementation/EngineerImplementation.cs
@@ -1,7 +1,9 @@
 ﻿namespace BlImplementation;
 using BlApi;
+using BO;
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 internal class EngineerImplementation : IEngineer
 {
@@ -10,6 +12,15 @@ internal class EngineerImplementation : IEngineer
     public void Create(BO.Engineer? engineer)
     {
         try
+        {
+            if (engineer == null)
+                throw new BlNullPropertyException("");
+            if (engineer.Id <= 0 || engineer.Name == "" || engineer.Cost <= 0 || !CheckEmail(engineer.Email))
+                throw new BlInvalidValueException("engineer with invalid values");
+
+            _dal.Engineer.Create(new DO.Engineer(engineer.Id, (DO.EngineerExperience)engineer.Level, engineer.Email, engineer.Cost, engineer.Name));
+        }
+        catch (Exception ex)
         {
 
         }
@@ -33,5 +44,15 @@ internal class EngineerImplementation : IEngineer
     public void Update(BO.Engineer? engineer)
     {
         throw new NotImplementedException();
+    }
+
+    bool CheckEmail(string email)
+    {
+        // Basic regular expression for email validation
+        string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+        // Create a Regex object
+        Regex regex = new Regex(pattern);
+        // Use the IsMatch method to validate the email
+        return regex.IsMatch(email);
     }
 }
