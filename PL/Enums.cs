6dc5c641cbfd51c7c8a795b0f2a6992d9591d28f
EstiRosen <estirphoto@gmail.com>
2024-02-19 17:11:59 +0200

    filters for tasks

--- PL/Enums.cs
+++ PL/Enums.cs
@@ -19,4 +19,12 @@ internal class EngineerCollection : IEnumerable
     public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
 }
 
+internal class StatusCollection : IEnumerable
+{
+    static readonly IEnumerable<BO.Status> s_enums =
+            (Enum.GetValues(typeof(BO.Status)) as IEnumerable<BO.Status>)!;
+
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
+
 
